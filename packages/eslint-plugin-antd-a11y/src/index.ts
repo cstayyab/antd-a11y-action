99fcbbd4ee571d7edbab0166eace065958396e0b
Muhammad Tayyab Sheikh <me@cstayyab.com>
2026-09-24 07:50:35 +0000

    Release 0.9.3

--- packages/eslint-plugin-antd-a11y/src/index.ts
+++ packages/eslint-plugin-antd-a11y/src/index.ts
@@ -14,7 +14,7 @@ export type { AntdA11yDocs, Impact } from './utils/create-rule.js';
 export { aliasErrors, aliasWarnings, type AliasMap, type AliasSpec } from './utils/aliases.js';
 
 export const PLUGIN_NAME = 'antd-a11y';
-export const VERSION = '0.9.2';
+export const VERSION = '0.9.3';
 
 export const rules = {
   'icon-button-has-name': iconButtonHasName,
